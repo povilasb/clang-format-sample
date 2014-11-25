a87e241a3a70cc15cb12afe44738585126c8f4a6
Povilas Balciunas <balciunas90@gmail.com>
2014-11-25 10:47:31 +0200

    Added sample source file.

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,21 @@
 #include <string>
 
 
+void 				help (	)	const;
+
+
 void
 nprint(std::string str, int n)
 {
+	const char *pstr = str.c_str();
 	for(int i=0;i<n;++i){
-				std::cout<<str;
+				std::cout<<str; // Simply output the specified string to stdout. This is way too long comment.
 	std::cout<<'\n';
 	}
 }
 int main() {
 	std::cout << "Some sample ill formated c++ code\n";
 	nprint("test");
+call_to_non_existant_function();
 return 0;
 }
